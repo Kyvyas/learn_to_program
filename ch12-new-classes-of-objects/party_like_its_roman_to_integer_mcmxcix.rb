3b61452b151774e1b186e773c9b7f839efec9920
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-07-14 21:10:48 +0000

    Added solution to roman num

--- ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
+++ ch12-new-classes-of-objects/party_like_its_roman_to_integer_mcmxcix.rb
@@ -1,3 +1,31 @@
 def roman_to_integer roman
-  # your code here
+  digits = {'i' => 1, 'v' => 5, 'x' => 10, 'l' => 50, 'c' => 100, 'd' => 500, 'm' => 1000}
+  
+total = 0
+  prev = 0
+  index = roman.length - 1
+  while index >= 0
+    x = roman[index].downcase
+    index = index - 1
+    val = digits[x]
+    if !val
+      puts "This is not valid"
+      return
+    end
+    
+    if val < prev
+      val = val * -1
+    else 
+      prev = val
+    end
+    total = total + val
+  end
+  
+  total
+end
+
+puts(roman_to_integer('mcmxcix'))
+
+puts(roman_to_integer('CCCLXV'))
+  while
 end
